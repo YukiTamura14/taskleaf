3208ac2b53151e76419a5a572b3b756c2497ff3c
yuji91 <yuji91.1227@gmail.com>
2019-09-16 14:20:10 +0900

    Add test cases

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :system do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'Task index' do
+    it 'enables user to index task list' do; end
+  end
+
+  describe 'Task show' do
+    it 'enables user to show task detail' do; end
+  end
+
+  describe 'Task new create' do
+    it 'enables user to create new task' do; end
+  end
+
+  describe 'Task edit update' do
+    it 'enables user to update task' do; end
+  end
+
+  describe 'Task destroy' do
+    it 'enables user to destroy task' do; end
+  end
 end
